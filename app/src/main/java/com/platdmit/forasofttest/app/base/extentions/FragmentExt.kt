962ec7df8a9d2f50a262ef441b99c31e302682dd
DmitriyPlatonov <platdm-it@yandex.ru>
2020-10-13 18:10:31 +0300

    fix| hide empty fun

--- app/src/main/java/com/platdmit/forasofttest/app/base/extentions/FragmentExt.kt
+++ app/src/main/java/com/platdmit/forasofttest/app/base/extentions/FragmentExt.kt
@@ -1,10 +1,7 @@
 package com.platdmit.forasofttest.app.base.extentions
 
-import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
-import androidx.paging.LoadState
 import com.google.android.material.snackbar.Snackbar
-import com.platdmit.forasofttest.R
 import com.platdmit.forasofttest.app.utilities.ProgressBarHandler
 
 /**
@@ -13,17 +10,6 @@ import com.platdmit.forasofttest.app.utilities.ProgressBarHandler
 fun Fragment.showResultMessage(message: String) {
     view?.let {
         Snackbar.make(it, message, Snackbar.LENGTH_LONG).show()
-    }
-}
-
-@SuppressLint("ResourceType")
-fun Fragment.showResultMessageButton(message: String) {
-    view?.let {
-        Snackbar.make(it, message, Snackbar.LENGTH_LONG)
-            .setAction(R.string.retry_button) {
-
-            }
-            .show()
     }
 }
 
