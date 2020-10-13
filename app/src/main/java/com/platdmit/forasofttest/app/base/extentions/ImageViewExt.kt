b9a9caa37f87b323421093b2803db6d751ebcd6d
DmitriyPlatonov <platdm-it@yandex.ru>
2020-10-13 22:34:45 +0300

    add| hover for albums in list

--- app/src/main/java/com/platdmit/forasofttest/app/base/extentions/ImageViewExt.kt
+++ app/src/main/java/com/platdmit/forasofttest/app/base/extentions/ImageViewExt.kt
@@ -1,11 +1,13 @@
 package com.platdmit.forasofttest.app.base.extentions
 
 import android.widget.ImageView
+import com.platdmit.forasofttest.R
 import com.squareup.picasso.Picasso
 
 fun ImageView.picassoRender(url: String) {
     Picasso
         .get()
         .load(url)
+        .placeholder(R.mipmap.ic_album_hover)
         .into(this)
 }
