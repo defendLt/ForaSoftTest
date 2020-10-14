166f63d1a649f9fa880b759e62a15d304a80635a
DmitriyPlatonov <platdm-it@yandex.ru>
2020-10-14 15:25:10 +0300

    add| new ext fun for show message

--- app/src/main/java/com/platdmit/forasofttest/app/base/extentions/FragmentExt.kt
+++ app/src/main/java/com/platdmit/forasofttest/app/base/extentions/FragmentExt.kt
@@ -2,14 +2,27 @@ package com.platdmit.forasofttest.app.base.extentions
 
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
+import com.platdmit.forasofttest.R
 import com.platdmit.forasofttest.app.utilities.ProgressBarHandler
 
 /**
- * Base handler for alert messages
+ * Base handlers for alert messages
  **/
 fun Fragment.showResultMessage(message: String) {
     view?.let {
         Snackbar.make(it, message, Snackbar.LENGTH_LONG).show()
+    }
+}
+fun Fragment.showResultMessage(messageId: Int) {
+    view?.let {
+        Snackbar.make(it, messageId, Snackbar.LENGTH_LONG).show()
+    }
+}
+fun Fragment.showActionResultMessage(message: String, action: () -> Unit) {
+    view?.let {
+        val actSnackbar =  Snackbar.make(it, message, Snackbar.LENGTH_INDEFINITE)
+        actSnackbar.setAction(R.string.retry_button){ action.invoke() }
+        actSnackbar.show()
     }
 }
 
